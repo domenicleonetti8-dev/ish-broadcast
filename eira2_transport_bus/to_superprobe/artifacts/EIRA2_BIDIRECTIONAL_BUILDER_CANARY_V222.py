78f2e9de7c1ff4c0b952725d4468556a4db0769a
domenicleonetti8-dev <domenicleonetti8@gmail.com>
2026-09-09 02:32:41 -0400

    Add V2.2.2 isolated Builder canary payload

---
+++ eira2_transport_bus/to_superprobe/artifacts/EIRA2_BIDIRECTIONAL_BUILDER_CANARY_V222.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python3
+CANARY_SCHEMA = "eira2_bidirectional_builder_canary_v1"
+CANARY_STATUS = "PASS"
+CANARY_PURPOSE = "prove_chatgpt_github_ext4_watcher_builder_easystore_return_path"
